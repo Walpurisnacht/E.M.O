da995e1e18378d3f5b045a94fdb37dce924c6898
Walpurisnacht <walpurisnacht.klarissa@gmail.com>
2016-07-03 01:27:44 +0700

    [Update] - Chapter 3 complete - Advanced command line argument processor [TODO] - Update app mode - Fix paper

--- src/cam_overlay.h
+++ src/cam_overlay.h
@@ -11,3 +11,48 @@
 using namespace std;
 using namespace dlib;
 
+struct arg_param {
+    char* face_model = (char*)"model.dat";
+    char* svm_model = (char*)"600.model";
+    char* neutral = (char*)"neutral.csv";
+    char* rec_path = (char*)"./";
+    bool record = false;
+};
+
+void exit_with_help()
+{
+    cout << "Usage: ./cam_overlay [options]\n"
+         << "Options:\n"
+         << "-s : data in different locations, set path to [face_model] [svm_model] [neutral.csv]\n"
+         << "-r : toggle frame extractor, set path to save image\n"
+         << "Default: data in current working dir, no recording" << endl;
+    exit(1);
+}
+
+void parse_command_line(int argc, char** argv, arg_param &app_arg)
+{
+    int i;
+    for (i = 1; i < argc; i++) {
+        if (argv[i][0] != '-') break;
+        if (++i >= argc) exit_with_help();
+
+        switch(argv[i-1][1])
+        {
+            case 's':
+                app_arg.face_model = argv[i];
+                app_arg.svm_model = argv[i+1];
+                app_arg.neutral = argv[i+2];
+                i+=2;
+                break;
+
+            case 'r':
+                app_arg.rec_path = argv[i];
+                app_arg.record = true;
+                break;
+
+            default:
+                cout << "Unknown options" << endl;
+                exit_with_help();
+        }
+    }
+}
